8367bafec5ccc1998a1821aba3b4c32fbfbf6c4c
jondesk <applebyjon@gmail.com>
2023-08-19 11:38:13 -0400

    update with all draft picks

--- draft_detail.py
+++ draft_detail.py
@@ -1,3 +1,4 @@
+import json
 import pandas as pd
 import requests
 from setup_info import SWID, ESPN_S2, LEAGUE_ID
@@ -5,30 +6,62 @@ from setup_info import SWID, ESPN_S2, LEAGUE_ID
 
 def get_draft_data(year):
     url = f'https://fantasy.espn.com/apis/v3/games/ffl/seasons/{year}/segments/0/' \
-                  f'leagues/{LEAGUE_ID}?view=mMatchup&view=mDraftDetail'
+          f'leagues/{LEAGUE_ID}?view=mMatchup&view=mDraftDetail'
     req = requests.get(url, cookies={"SWID": SWID, "espn_s2": ESPN_S2})
     data = req.json()
 
+    draft = []
     picks = data['draftDetail']['picks']
     for pick in picks:
-        team = pick['teamId']
-        player_id = pick['playerId']
-        player_name = ''
-        if player_id in player_map:
-            player_name = player_map[player_id]
-        round_num = pick['roundId']
-        round_pick = pick['roundPickNumber']
-        bid_amount = pick['bidAmount']
-        keeper_status = pick['keeper']
+        pick_details = {'team': pick['teamId'],
+                        'player_id': pick['playerId'],
+                        'round_num': pick['roundId'],
+                        'round_pick': pick['roundPickNumber'],
+                        'overall_pick': pick['overallPickNumber']
+                        }
+        draft.append(pick_details)
+
+    return draft
 
 
 def get_player_card(year):
     url = f'https://fantasy.espn.com/apis/v3/games/ffl/seasons/{year}/segments/0/' \
           f'leagues/{LEAGUE_ID}?view=kona_playercard'
-    req = requests.get(url, cookies={"SWID": SWID, "espn_s2": ESPN_S2})
+
+    filters = {"players": {"limit": 1500,
+                           "sortDraftRanks": {
+                               "sortPriority": 100,
+                               "sortAsc": True,
+                               "value": "STANDARD"
+                           }
+                           }
+               }
+    headers = {'x-fantasy-filter': json.dumps(filters)}
+
+    req = requests.get(url, cookies={"SWID": SWID, "espn_s2": ESPN_S2}, headers=headers)
     data = req.json()
+
+    players = []
+    player_info = data['players']
+    for player in player_info:
+        ind_player = player['player']
+        player_details = {'player_id': ind_player['id'],
+                          'first_name': ind_player['firstName'],
+                          'last_name': ind_player['lastName']
+                          }
+        players.append(player_details)
+
+    return players
+
+
+def combine_draft_player(year):
+    draft = pd.DataFrame(get_draft_data(year))
+    player = pd.DataFrame(get_player_card(year))
+
+    draft = draft.merge(player, how='left', left_on='player_id', right_on='player_id')
+
+    print(draft)
 
 
 year = 2022
-get_draft_data(year)
-get_player_card(year)
+combine_draft_player(year)
