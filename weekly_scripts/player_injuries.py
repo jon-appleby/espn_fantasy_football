b4b310b9c840f711c26ce37f9ec7beddd41cbd01
jon-laptop <applebyjon@gmail.com>
2024-05-18 20:16:36 -0400

    begin dev injuries

--- weekly_scripts/player_injuries.py
+++ weekly_scripts/player_injuries.py
@@ -3,21 +3,21 @@ from main.espn_api import fetch_api_data
 
 def get_teams(curr_year, curr_week):
     data = fetch_api_data(views=['mMatchup'],
-                          params={'matchupPeriodId': curr_week, 'scoringPeriodId': curr_week},
+                          params={'matchupPeriodId': curr_week, 'scoringPeriodId': week},
                           year=curr_year)
     return data['teams']
 
 
 def get_boxscore(curr_year, curr_week):
     data = fetch_api_data(views=['mBoxscore'],
-                          params={'matchupPeriodId': curr_week, 'scoringPeriodId': curr_week},
+                          params={'matchupPeriodId': curr_week, 'scoringPeriodId': week},
                           year=curr_year)
     return data['teams']
 
 
-def get_player_info(curr_year, curr_week):
-    data = fetch_api_data(views=['kona_player_info'],
-                          params={'matchupPeriodId': curr_week, 'scoringPeriodId': curr_week},
+def get_players(curr_year, curr_week):
+    data = fetch_api_data(views=['mMatchup','mMatchupScore'],
+                          params={'matchupPeriodId': curr_week},
                           year=curr_year)
     return data
 
@@ -33,27 +33,36 @@ def get_team_ids(team_input) -> dict[int, str]:
     return team_dict
 
 
-def team_detail(team_input):
-    for team in team_input:
-        team_id = team['id']
-        roster = team['roster']['entries']
-        if team_id == 6:
-            for player in roster:
-                player_detail = player['playerPoolEntry']['player']
-                player_name = player_detail['fullName']
-                player_status = player_detail.get('injuryStatus', '')
+# def team_detail(team_input):
+#     for team in team_input:
+#         team_id = team['id']
+#         roster = team['roster']['entries']
+#         if team_id == 6:
+#             for player in roster:
+#                 player_detail = player['playerPoolEntry']['player']
+#                 player_name = player_detail['fullName']
+#                 player_status = player_detail.get('injuryStatus', '')
+
+def player_detail(player_input):
+    for matchup in player_input["schedule"]:
+        home_team = matchup["home"]["teamId"]
+        away_team = matchup["away"]["teamId"]
+
+        for team_id in [home_team, away_team]:
+            for player in matchup["teams"][str(team_id)]["roster"]["entries"]:
+                player_name = player["playerPoolEntry"]["player"]["fullName"]
+                player_injury_status = player["playerPoolEntry"]["player"]["injuryStatus"]
+
+                print(f"{player_name}: {player_injury_status}")
 
 
 year = 2023
 week = 4
 
-# team_data = get_boxscore(year, week)
-# get_team_ids(team_data)
-#
-# player_data = get_teams(year, week)
-# team_detail(player_data)
+team_data = get_boxscore(year, week)
+get_team_ids(team_data)
 
-print(get_player_info(year, week))
-
+player_data = get_players(year, week)
+player_detail(player_data)
 
 # get starting players
