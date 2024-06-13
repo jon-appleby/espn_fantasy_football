f493f8ee7f46898006939e7e8929a4980ab134ec
jondesk <applebyjon@gmail.com>
2024-06-13 19:39:05 -0400

    player info/injury testing

--- weekly_scripts/player_injuries.py
+++ weekly_scripts/player_injuries.py
@@ -1,40 +1,58 @@
+import pandas as pd
 from main.espn_api import fetch_api_data
+from time import sleep
 
-
-def get_teams(curr_year, curr_week):
-    data = fetch_api_data(views=['mMatchup'],
-                          params={'matchupPeriodId': curr_week, 'scoringPeriodId': curr_week},
-                          year=curr_year)
-    return data['teams']
-
-
-def get_boxscore(curr_year, curr_week):
-    data = fetch_api_data(views=['mBoxscore'],
-                          params={'matchupPeriodId': curr_week, 'scoringPeriodId': curr_week},
-                          year=curr_year)
-    return data['teams']
+# def get_teams(curr_year, curr_week):
+#     data = fetch_api_data(views=['mMatchup'],
+#                           params={'matchupPeriodId': curr_week, 'scoringPeriodId': curr_week},
+#                           year=curr_year)
+#     return data['teams']
+#
+#
+# def get_boxscore(curr_year, curr_week):
+#     data = fetch_api_data(views=['mBoxscore'],
+#                           params={'matchupPeriodId': curr_week, 'scoringPeriodId': curr_week},
+#                           year=curr_year)
+#     return data['teams']
 
 
 def get_player_info(curr_year, curr_week):
-    data = fetch_api_data(views=['kona_player_info'],
-                          params={'matchupPeriodId': curr_week, 'scoringPeriodId': curr_week},
-                          year=curr_year)
+    params = {'scoringPeriodId': curr_week, 'limit': 50, 'startIndex': 0}
+    header = {"x-fantasy-filter": '{"players":{"limit":1500}}'}
+    all_players = []
+
+    while True:
+        data = fetch_api_data(views=['kona_player_info'],
+                              params=params,
+                              year=curr_year,
+                              header=header)
+
+        p = data.get('players', [])
+        all_players.extend(p)
+
+        if len(p) < params['limit']:
+            break
+
+        params['startIndex'] += params['limit']
+
+        sleep(2)
+
     return data
 
 
-def get_roster_info(curr_year, curr_week):
-    return
-
-
-def get_team_ids(team_input) -> dict[int, str]:
-    # create team dict
-    team_dict = {}
-    for team in team_input:
-        team_id = team['id']
-        name = team['name']
-        team_dict[team_id] = name
-
-    return team_dict
+# def get_roster_info(curr_year, curr_week):
+#     return
+#
+#
+# def get_team_ids(team_input) -> dict[int, str]:
+#     # create team dict
+#     team_dict = {}
+#     for team in team_input:
+#         team_id = team['id']
+#         name = team['name']
+#         team_dict[team_id] = name
+#
+#     return team_dict
 
 
 def team_detail(team_input):
@@ -54,9 +72,20 @@ week = 5
 # team_data = get_boxscore(year, week)
 # get_team_ids(team_data)
 
-player_data = get_teams(year, week)
-print(player_data)
+# player_data = get_teams(year, week)
+# print(player_data)
 
 # team_detail(player_data)
-#
-# print(get_player_info(year, week))
+
+player_data = get_player_info(year, week)
+players = player_data.get('players', [])
+injury_status_list = []
+
+for player in players:
+    injury_status = player['player'].get('injuryStatus', 'n/a')
+    full_name = player['player'].get('fullName', 'N/A')
+    injury_status_list.append({'player_name': full_name, 'injury_status': injury_status})
+
+df = pd.DataFrame(injury_status_list)
+
+print(df)
